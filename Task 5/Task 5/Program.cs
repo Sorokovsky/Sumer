accaac1f41d076fcafdfdbd67e23f88e498e404e
Sorokovsky <Sorokovskys@ukr.net>
2024-06-30 22:34:36 +0300

    task 5

---
+++ Task 5/Task 5/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace Task_5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a, b, c;
+            Console.WriteLine("Введіть сторони трикутника");
+            Console.Write("a = "); a = Convert.ToInt32(Console.ReadLine());
+            Console.Write("b = "); b = Convert.ToInt32(Console.ReadLine());
+            Console.Write("c = "); c = Convert.ToInt32(Console.ReadLine());
+            MaxWall(ref a, ref b, ref c);
+            if (c * c > a * a + b * b)
+            {
+                Console.WriteLine("Трикутник тупокутній");
+            }
+            else if (c * c == a * a + b * b)
+            {
+                Console.WriteLine("Трикутник Прямокутній");
+            }
+            else
+            {
+                Console.WriteLine("Трикутник Гострокутній");
+            }
+            Console.ReadLine();
+        }
+
+        static void MaxWall(ref int a,  ref int b, ref int c)
+        {
+            if(a > b && a > c)
+            {
+                Swap(ref a, ref c);
+            }
+            else if(b > a && b > c)
+            {
+                Swap(ref b, ref c);
+            }
+        }
+
+        static void Swap(ref int a, ref int b)
+        {
+            int temp = a;
+            a = b;
+            b = temp;
+        }
+
+    }
+}
