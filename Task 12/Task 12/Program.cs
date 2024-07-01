31fdf5e17e217db5a6325ce4a939f28dafb326fd
Sorokovsky <Sorokovskys@ukr.net>
2024-07-01 16:13:13 +0300

    task 12

---
+++ Task 12/Task 12/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Task_12
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = 10, m = 10;
+            int[,] array = new int[n, m];
+            for(int i = 0; i < n; i++)
+            {
+                for(int j = 0; j < m; j++)
+                {
+                    array[i, j] = (i + 1) * (j + 1);
+                }
+            }
+            Console.WriteLine("Таблиця Піфагора");
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < m; j++)
+                {
+                    Console.Write(array[i, j] + " ");
+                }
+                Console.WriteLine();
+            }
+            Console.ReadLine();
+        }
+    }
+}
