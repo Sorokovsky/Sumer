e9c6b19eacaff3dc6074b72855e41c6269ef3d6a
Sorokovsky <Sorokovskys@ukr.net>
2024-07-01 15:59:37 +0300

    task 11

---
+++ Task 11/Task 11/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+// Variant 19
+namespace Task_11
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n;
+            Console.Write("Введіть кількість елементів: "); n = Convert.ToInt32(Console.ReadLine());
+            if(n < 1)
+            {
+                n = 1;
+            }
+            int[] array = new int[n];
+            Random random = new Random();
+            for(int i = 0; i < array.Length; i++)
+            {
+                array[i] = random.Next(0, 100);
+            }
+            Console.WriteLine("Масив: " + string.Join(", ", array));
+            Console.WriteLine("Сума парних елементів: " + EvenSum(array));
+            Console.WriteLine("Сума не парних елементів: " + OddSum(array));
+            Console.WriteLine("Сума елементів які діляться на 3: " + CanDivide3Sum(array));
+            Console.WriteLine("");
+            Console.ReadLine();
+        }
+
+        static int EvenSum(int[] array)
+        {
+            int sum = 0;
+            foreach(int item in array)
+            {
+                if(item % 2 == 0)
+                {
+                    sum += item;
+                }
+            }
+            return sum;
+        }
+
+        static int OddSum(int[] array)
+        {
+            int sum = 0;
+            foreach (var item in array)
+            {
+                if(item % 2 != 0)
+                {
+                    sum += item;
+                }
+            }
+            return sum;
+        }
+
+        static int CanDivide3Sum(int[] array)
+        {
+            int sum = 0;
+            foreach (var item in array)
+            {
+                if (item % 3 == 0)
+                {
+                    sum += item;
+                }
+            }
+            return sum;
+        }
+    }
+}
